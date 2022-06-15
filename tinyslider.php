6ae2b18f174aac4ddc8d4e1b61817e81bde993c1
Sohan Chowdhury <89298094+prosohan7@users.noreply.github.com>
2022-06-15 11:10:05 +0600

    9.1

--- tinyslider.php
+++ tinyslider.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       TinySlider
- * Plugin URI:        https://example.com/plugins/qrcode/
+ * Plugin URI:        https://example.com/plugins/tinyslider/
  * Description:       This is practise plugin.
  * Version:           1.0
  * Author:            Sohan
@@ -19,6 +19,15 @@ function tinys_load_textdomain(){
 }
 add_action( 'plugin_loaded', 'tinys_load_textdomain' );
 
+
+// CSS and JS
+function tinys_assets(){
+    wp_enqueue_style( 'tinyslider-css', "//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css", null, '1.0' );
+    wp_enqueue_script( 'tinyslider-js', "//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js", array('jquery'), time(), true );
+    wp_enqueue_script( 'tinyslider-main-js', plugin_dir_url( __FILE__ ) . "/assets/js/tinyslider-main.js", array('jquery'), time(), true );
+}
+add_action( 'wp_enqueue_scripts', 'tinys_assets' );
+
 function tinys_shortcode_tslider( $args, $content ) {
     $defaults = array(
         'width' => 800,
@@ -28,8 +37,20 @@ function tinys_shortcode_tslider( $args, $content ) {
     $attributes = shortcode_atts( $defaults, $args );
     $content = do_shortcode( $content );
 
-    $shortcode_output = 'test output';
+    $shortcode_output = '
+        <div id="{$attributes[id]}" style="width:{$attributes[width]}; height={$attributes[height]}">
+            <div class="slider">
+                {$content}
+            </div>
+        </div>
+    ';
 
     return $shortcode_output;
 }
 add_shortcode( 'tslider', 'tinys_shortcode_tslider' );
+
+// 11.00 mins
+
+
+
+
